663dc774d9f1098ebfd286212a8bb2eade76f597
Peter Nagy <peter.nagy@prezi.com>
2020-12-23 23:56:01 +0100

    add save state and restore state in the actrivity

--- app/src/main/java/com/petnagy/customviewdemo/MainActivity.kt
+++ app/src/main/java/com/petnagy/customviewdemo/MainActivity.kt
@@ -2,9 +2,21 @@ package com.petnagy.customviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.petnagy.customviewdemo.common.DemoView
 import kotlinx.android.synthetic.main.activity_main.demoButton
+import kotlinx.android.synthetic.main.activity_main.demoDemo1
+import kotlinx.android.synthetic.main.activity_main.demoDemo2
+import kotlinx.android.synthetic.main.activity_main.demoDemo3
+import kotlinx.android.synthetic.main.activity_main.demoDemo4
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    companion object {
+        private const val VIEW1_STATE = "view1State"
+        private const val VIEW2_STATE = "view2State"
+        private const val VIEW3_STATE = "view3State"
+        private const val VIEW4_STATE = "view4State"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +24,51 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             val intent = SecondActivity.createLaunchIntent(this)
             startActivity(intent)
         }
+//        if (savedInstanceState != null) {
+//            val view1State = savedInstanceState.getParcelable(VIEW1_STATE) as DemoView.DemoViewState?
+//            val view2State = savedInstanceState.getParcelable(VIEW2_STATE) as DemoView.DemoViewState?
+//            val view3State = savedInstanceState.getParcelable(VIEW3_STATE) as DemoView.DemoViewState?
+//            val view4State = savedInstanceState.getParcelable(VIEW4_STATE) as DemoView.DemoViewState?
+//            view1State?.let {
+//                demoDemo1.setUiState(it)
+//            }
+//            view2State?.let {
+//                demoDemo2.setUiState(it)
+//            }
+//            view3State?.let {
+//                demoDemo3.setUiState(it)
+//            }
+//            view4State?.let {
+//                demoDemo4.setUiState(it)
+//            }
+//        }
     }
+
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putParcelable(VIEW1_STATE, demoDemo1.getUiState())
+//        outState.putParcelable(VIEW2_STATE, demoDemo2.getUiState())
+//        outState.putParcelable(VIEW3_STATE, demoDemo3.getUiState())
+//        outState.putParcelable(VIEW4_STATE, demoDemo4.getUiState())
+//    }
+
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        val view1State = savedInstanceState.getParcelable(VIEW1_STATE) as DemoView.DemoViewState?
+//        val view2State = savedInstanceState.getParcelable(VIEW2_STATE) as DemoView.DemoViewState?
+//        val view3State = savedInstanceState.getParcelable(VIEW3_STATE) as DemoView.DemoViewState?
+//        val view4State = savedInstanceState.getParcelable(VIEW4_STATE) as DemoView.DemoViewState?
+//        view1State?.let {
+//            demoDemo1.setUiState(it)
+//        }
+//        view2State?.let {
+//            demoDemo2.setUiState(it)
+//        }
+//        view3State?.let {
+//            demoDemo3.setUiState(it)
+//        }
+//        view4State?.let {
+//            demoDemo4.setUiState(it)
+//        }
+//    }
 }
--- app/src/main/java/com/petnagy/customviewdemo/common/DemoView.kt
+++ app/src/main/java/com/petnagy/customviewdemo/common/DemoView.kt
@@ -1,11 +1,14 @@
 package com.petnagy.customviewdemo.common
 
 import android.content.Context
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.petnagy.customviewdemo.R
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.view_demo.view.demoCheckBox
+import kotlinx.android.synthetic.main.view_demo.view.demoInputField
 import kotlinx.android.synthetic.main.view_demo.view.demoLabel
 import kotlinx.android.synthetic.main.view_demo.view.demoRadio
 
@@ -54,4 +57,34 @@ class DemoView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         demoRadio.isChecked = checked
     }
 
+    fun isRadioButtonOn() = demoRadio.isChecked
+
+    fun isCheckBoxOn() = demoCheckBox.isChecked
+
+    fun getText() = demoInputField.text.toString()
+
+    fun setRadioButtonState(checked: Boolean) {
+        demoRadio.isChecked = checked
+    }
+
+    fun setCheckBoxState(checked: Boolean) {
+        demoCheckBox.isChecked = checked
+    }
+
+    fun setText(text: String) {
+        demoInputField.setText(text)
+    }
+
+    fun getUiState(): DemoViewState {
+        return DemoViewState(demoRadio.isChecked, demoCheckBox.isChecked, demoInputField.text.toString())
+    }
+
+    fun setUiState(uiState: DemoViewState) {
+        demoRadio.isChecked = uiState.radioButtonChecked
+        demoCheckBox.isChecked = uiState.checkBoxChecked
+        demoInputField.setText(uiState.text)
+    }
+
+    @Parcelize
+    data class DemoViewState(val radioButtonChecked: Boolean, val checkBoxChecked: Boolean, val text: String): Parcelable
 }
